a146a50325bff2891a196a2e7bddf70c6e792085
Osman <32338711+Osman-Gone@users.noreply.github.com>
2017-11-18 12:17:31 +0600

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,24 @@
+if(.htaccess){
+
+# BEGIN WordPress
+<IfModule mod_rewrite.c>
+RewriteEngine On
+RewriteBase /Engilsh-theme-development/
+RewriteRule ^index\.php$ - [L]
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteRule . /Engilsh-theme-development/index.php [L]
+</IfModule>
+
+
+php_value upload_max_filesize 64M
+php_value post_max_size 64M
+php_value max_execution_time 300
+php_value max_input_time 300
+
+# END WordPress
+}else{
+upload_max_filesize = 64M
+post_max_size = 64M
+max_execution_time = 300
+}
